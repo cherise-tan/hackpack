4cb3569f12ef4949900b273ee945d2cbf71bf77d
Carl Bennett <carlbennettnz@gmail.com>
2019-06-01 12:37:22 +1200

    Display correct list based on forecast

--- src/app.jsx
+++ src/app.jsx
@@ -4,56 +4,53 @@ import React, {  useState} from 'react'
 import { render } from 'react-dom'
 
 const App = () => {
-  const [temp, setTemp] = useState(null);
   const [destination, setDestination] = useState("");
+  const [forecast, setForecast] = useState([]);
 
   const submit = (event) => {
     event.preventDefault();
-    console.log(destination);
+    fetch(`https://api.apixu.com/v1/forecast.json?key=d2c6a897b72845b08cb02436190106&q=${encodeURIComponent(destination)}`)
+      .then(res => res.json())
+      .then(weather => weather.forecast.forecastday.map(day => ({
+        date: day.date,
+        temp: day.day.avgtemp_c,
+      })))
+      .then(newForecast => setForecast(newForecast))
   }
 
-  if (!temp) {
-    return <Form setTemp={setTemp} submit={submit} setDestination={setDestination}/>
-
+  if (!forecast.length) {
+    return <Form submit={submit} setDestination={setDestination}/>
   }
 
-  return <Pack temp={temp} />
+  return <Pack temp={forecast[0].temp} />
 }
 
 const Form = (props) => {
   return (
     <form onSubmit = {props.submit}>
-
-    <div>
-      <label>I am going to: </label>
-      <input id="destination" type="text" placeholder="Destination" onChange={(event) => props.setDestination(event.target.value)}/>
-    </div>
-    <div>
-      <label>Date of travel: </label>
-      <input id="date" type="date" placeholder="Date"/>
-    </div>
-    <div>
-      <label>Travel Style: </label>
-      <button>Pack Light</button>
-      <button>Pack Heavy</button>
-    </div>
-    <div>
-      <button  type="submit" value="submit">Submit</button>
-    </div>
-
-    <div>
-      <button onClick = { () => props.setTemp("hot") } >Hot</button>
-      <button onClick = { () => props.setTemp("mild") } >Mild</button>
-      <button onClick = { () => props.setTemp("cold") } >Cold</button>
-
-    </div>
-    </form >
+      <div>
+        <label>I am going to: </label>
+        <input id="destination" type="text" placeholder="Destination" onChange={(event) => props.setDestination(event.target.value)}/>
+      </div>
+      <div>
+        <label>Date of travel: </label>
+        <input id="date" type="date" placeholder="Date"/>
+      </div>
+      <div>
+        <label>Travel Style: </label>
+        <button>Pack Light</button>
+        <button>Pack Heavy</button>
+      </div>
+      <div>
+        <button  type="submit" value="submit">Submit</button>
+      </div>
+    </form>
   )
 }
 
-const Pack = ({temp}) => {
-  if (temp === 'hot') return <PackHot />
-  if (temp === 'cold') return <PackCold />
+const Pack = ({ temp }) => {
+  if (temp > 24) return <PackHot />
+  if (temp < 15) return <PackCold />
   return <PackMild />
 }
 

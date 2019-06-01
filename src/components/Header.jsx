b7608727b9d884fbce4748123ab1c63cb2c54dd3
April <apriljh.liau@gmail.com>
2019-06-01 16:54:38 +1200

    button space

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
 
-import icon from '../images/icon.png'
-
 const Header = (props) => {
   return (
     <div className="row header">
       <div className="logo">Hack Pack</div>
-      <div className="menuicon"><img src={icon} /></div>
     </div>
   )
 }
